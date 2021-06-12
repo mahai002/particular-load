32c3ee03f72a914137a891138fa8fd0dfaee37af
Махир <mahai002>
2021-06-13 03:00:23 +0500

    particular load

---
+++ script.js
@@ -0,0 +1,26 @@
+// $('.nav__link:first-of-type').addClass('active')
+// console.log($('.nav__link'));
+
+// var links = document.querySelectorAll('.nav__link');
+// links[0].classList.add('active')
+
+// links.addEventListener('click', function (e) {  
+    
+// })
+
+
+
+
+// $('.nav__link').on('click', function (e) {  
+//     e.preventDefault();
+//     $(this).addClass('active');
+//     $('.nav__link').not($(this)).removeClass('active')
+//     var url = $(this).attr('href')
+//     // console.log(url);
+//     var content = document.querySelector('.content');
+
+//     $('.main').load(url)
+
+
+// })
+
